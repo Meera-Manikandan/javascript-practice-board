e288953030f7d7df9c8c479d423ea5b1a5af84f6
Meera Manikandan <meeramnair@gmail.com>
2021-12-19 18:06:32 -0500

    Array Concat & CopyWithin Method usage

---
+++ 002_Array_Concat.js
@@ -0,0 +1,8 @@
+function func() {
+    var numArray1 = [11, 12, 13],
+        numArray2 = [14, 15, 16],
+        numArray3 = [17, 18, 19];
+    var concatArray = numArray1.concat(numArray2, numArray3)
+  console.log("Concatnated Array - " +concatArray);
+}
+func();
---
+++ 003_Array_CopyWithin.js
@@ -0,0 +1,6 @@
+var array = [1, 2, 3, 4, 5, 6, 7];
+
+//Syntax for copyWithin method : array.copyWithin(target, start, end)
+console.log("Copy Within Array " + array.copyWithin(0, 3, 6));
+
+//Ooutput : Copy Within Array 4,5,6,4,5,6,7
